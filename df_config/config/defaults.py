33d18dda417c88167dab96d34f8bf1dcbbc64644
d9pouces <github@19pouces.net>
2020-09-11 12:02:24 +0200

    better use of djt

--- df_config/config/defaults.py
+++ df_config/config/defaults.py
@@ -37,6 +37,7 @@ written in .ini files. The mapping between the Python setting and the [section/o
 """
 import os
 
+from df_config.guesses.djt import guess_djt_panels
 from django.utils.translation import gettext_lazy as _
 
 # ######################################################################################################################
@@ -329,20 +330,7 @@ ACCOUNT_ADAPTER = "df_config.apps.allauth.AccountAdapter"
 # Django-Debug-Toolbar
 DEBUG_TOOLBAR_CONFIG = {"JQUERY_URL": "{STATIC_URL}vendor/jquery/dist/jquery.min.js"}
 DEBUG_TOOLBAR_PATCH_SETTINGS = False
-DEBUG_TOOLBAR_PANELS = [
-    "debug_toolbar.panels.versions.VersionsPanel",
-    "debug_toolbar.panels.timer.TimerPanel",
-    "debug_toolbar.panels.settings.SettingsPanel",
-    "debug_toolbar.panels.profiling.ProfilingPanel",
-    "debug_toolbar.panels.headers.HeadersPanel",
-    "debug_toolbar.panels.request.RequestPanel",
-    "debug_toolbar.panels.sql.SQLPanel",
-    "debug_toolbar.panels.templates.TemplatesPanel",
-    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
-    "debug_toolbar.panels.cache.CachePanel",
-    "debug_toolbar.panels.signals.SignalsPanel",
-    "debug_toolbar.panels.redirects.RedirectsPanel",
-]
+DEBUG_TOOLBAR_PANELS = CallableSetting(guess_djt_panels)
 INTERNAL_IPS = ("127.0.0.1", "::1", "localhost")
 
 # django-auth-ldap
---
+++ df_config/guesses/djt.py
@@ -0,0 +1,31 @@
+from df_config.utils import is_package_present
+
+DEBUG_TOOLBAR_PANELS = [
+    "debug_toolbar.panels.versions.VersionsPanel",
+    "debug_toolbar.panels.timer.TimerPanel",
+    "debug_toolbar.panels.settings.SettingsPanel",
+    "debug_toolbar.panels.profiling.ProfilingPanel",
+    "debug_toolbar.panels.headers.HeadersPanel",
+    "debug_toolbar.panels.request.RequestPanel",
+    "debug_toolbar.panels.sql.SQLPanel",
+    "debug_toolbar.panels.templates.TemplatesPanel",
+    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+    "debug_toolbar.panels.cache.CachePanel",
+    "debug_toolbar.panels.signals.SignalsPanel",
+    "debug_toolbar.panels.redirects.RedirectsPanel",
+]
+
+
+def guess_djt_panels(settings_dict):
+    if not settings_dict["DEBUG"] or not settings_dict["USE_DEBUG_TOOLBAR"]:
+        return []
+    panels = list(DEBUG_TOOLBAR_PANELS)
+    if is_package_present("djt_nvu"):
+        panels.insert(2, "djt_nvu.panel.W3ValidatorPanel")
+    if is_package_present("djt_og"):
+        panels.insert(3, "djt_og.panel.OpenGraphPanel")
+
+    return panels
+
+
+guess_djt_panels.required_settings = ["DEBUG", "USE_DEBUG_TOOLBAR"]
